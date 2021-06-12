7be9e4c4e890bde16d1569047ba7f61ebb7be310
Suh Donghwi <hwidongsuh@gmail.com>
2021-06-12 09:28:22 +0900

    chore: remove unnecessary comments

--- client.c
+++ client.c
@@ -14,44 +14,26 @@
 
 void receive_server_addr(char *dest, char *multi_addr)
 {
-  /* 
-  
-    socket(DOMAIN, TYPE, PROTOCOL): 소켓을 생성하고 소켓 디스크립터를 반환합니다.
-    - DOMAIN : 어떤 영역에서 통신할 것인지를 결정합니다. PF_INET은 IPv4를 의미합니다.
-    - TYPE : 어떤 서비스 타입의 소켓을 생성할 것인지를 결정합니다. SOCK_DGRAM은 UDP, SOCK_STREAM은 TCP입니다.
-    - PROTOCOL : UDP, TCP를 정하는 부분인데 0을 넘기면 그냥 TYPE에서 정한 대로 따라갑니다.
-  */
   int multi_sock = socket(PF_INET, SOCK_DGRAM, 0);
 
-  struct sockaddr_in addr; // 소켓 주소에 대한 정보를 담는 구조체입니다.
+  struct sockaddr_in addr;
   memset(&addr, 0, sizeof(addr));
-  addr.sin_family = AF_INET;                // IPv4 형식으로 지정합니다.
-  addr.sin_addr.s_addr = htonl(INADDR_ANY); // IP를 지정합니다. htonl(INADDR_ANY)가 현재 시스템의 IP를 반환합니다.
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(PORT);
 
-  /*
-    setsockopt(...) : 소켓의 옵션을 설정합니다.
-    - SO_REUSEADDR : 지역 주소(IP 주소, 포트번호)의 재사용을 허용합니다.
-  */
   int on = 1;
   setsockopt(multi_sock, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
 
-  /* 
-    bind(SOCKFD, ADDR, ADDRLEN) : IP 주소와 소켓 디스크립터를 묶어서 외부에서 IP로 통신을 시도하는 컴퓨터가 소켓 디스크립터를 찾을 수 있도록 해줍니다. 실패하면 -1을 반환합니다.
-    - SOCKFD : 묶으려는 소켓입니다.
-    - ADDR : 서버 주소 정보입니다.
-    - ADDRLEN : 서버 주소 정보의 크기입니다.
-  */
   if (bind(multi_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
   {
     error_handle("bind() error");
   }
 
-  struct ip_mreq join_addr;                               // 멀티캐스트 통신을 위한 정보를 담는 구조체입니다.
-  join_addr.imr_multiaddr.s_addr = inet_addr(multi_addr); // 멀티캐스트 그룹의 IP 주소를 지정합니다.
-  join_addr.imr_interface.s_addr = htonl(INADDR_ANY);     // 멀티캐스트 패킷을 받을 네트워크 인터페이스를 지정합니다.
+  struct ip_mreq join_addr;
+  join_addr.imr_multiaddr.s_addr = inet_addr(multi_addr);
+  join_addr.imr_interface.s_addr = htonl(INADDR_ANY);
 
-  // setsockopt로 IP_ADD_MEMBERSHIP을 지정하면 소켓이 join_addr이 나타내는 멀티캐스트 그룹에 가입됩니다.
   if (setsockopt(multi_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)&join_addr,
                  sizeof(join_addr)) < 0)
   {
